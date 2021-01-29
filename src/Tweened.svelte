3c56a490115ad4a484a0d2152fbd8bcb8b31e909
ADVANTECH\Tinalin.lin <Tinalin.Lin@advantech.com.tw>
2021-01-29 08:59:36 +0800

    Add | Tweened.svelte

---
+++ src/Tweened.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { tweened } from 'svelte/motion';
+
+    let year = tweened(1990, { duration: 3000 });
+    console.log('year => ', year);
+
+    onMount(() => {
+        setTimeout(() => year.set(new Date().getFullYear()), 2000);
+    });
+</script>
+
+<h1>Hello {Math.floor($year)}</h1>
