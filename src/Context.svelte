5caa5cce10c13ee6aa17b1c13a14214f0d523371
ADVANTECH\Tinalin.lin <Tinalin.Lin@advantech.com.tw>
2021-01-29 08:59:09 +0800

    Add | Context.svelte

---
+++ src/Context.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { getContext } from 'svelte';
+    const user: {
+        name: string;
+        age: number;
+    } = getContext('user');
+</script>
+
+<h2>{user.name}</h2>
+<p>{user.age}</p>
+
+<style>
+</style>
