ac8b63320a5ea5edcf7c57ff3f4799a7bac0f001
ADVANTECH\Tinalin.lin <Tinalin.Lin@advantech.com.tw>
2021-01-29 08:57:42 +0800

    Add | Await.svelte

---
+++ src/Await.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { onMount, createEventDispatcher } from 'svelte';
+    const url: Promise<any> = fetch('https://api.github.com/user/7070587').then((res) => res.json());
+</script>
+
+{#await url}
+    <span>Loading.....</span>
+{:then res}
+    <img src={res.avatar_url} alt="" />
+    <h2>{res.id}</h2>
+    <h2>{res.created_at}</h2>
+{:catch error}
+    <span> {error} </span>
+{/await}
+
+<style>
+</style>
