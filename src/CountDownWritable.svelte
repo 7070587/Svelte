ff5b09007397153a90a41ff233aca119e8a5aa3f
ADVANTECH\Tinalin.lin <Tinalin.Lin@advantech.com.tw>
2021-01-29 08:58:04 +0800

    Add | CountDownWritable.svelte

---
+++ src/CountDownWritable.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    /*
+    1. props
+    2. if block
+    3. $
+    */
+
+    import { onMount } from 'svelte';
+    import { countdownTimer, setCountdown } from './CountDown';
+    import { from } from 'rxjs';
+
+    onMount(() => {
+        setCountdown(30);
+        setTimeout(() => setCountdown(10), 2000);
+    });
+
+    const observable = from(['from rxjs']);
+    observable.subscribe((x) => console.log('Message => ', x));
+</script>
+
+<h4>Observable: {$observable}</h4>
+
+<div>Count Down: {$countdownTimer}</div>
+
+<style>
+    div {
+        color: darkorchid;
+        font-weight: bold;
+        font-size: 1.5rem;
+    }
+
+    h4 {
+        color: darkred;
+    }
+</style>
