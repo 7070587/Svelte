7b786fcb994480aa59028d7777b8aaa164d9e362
ADVANTECH\Tinalin.lin <Tinalin.Lin@advantech.com.tw>
2021-02-01 10:23:58 +0800

    Add | Class.svelte

---
+++ src/Class.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    let isActive: boolean = false;
+
+    function handleClick() {
+        isActive = !isActive;
+        console.log('isActive => ', isActive);
+    }
+</script>
+
+<button class="btn" on:click={handleClick} class:active={isActive}> Click Me </button>
+
+<style>
+    .btn {
+        padding: 0.75rem 1rem;
+        border-radius: 6px;
+    }
+
+    .active {
+        background-color: darkturquoise;
+        color: floralwhite;
+        font-weight: bold;
+    }
+</style>
