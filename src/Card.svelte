6c525a13f233c4825f2be5d2cd834f8996e08b74
ADVANTECH\Tinalin.lin <Tinalin.Lin@advantech.com.tw>
2021-02-01 10:24:07 +0800

    Add | Card.svelte

---
+++ src/Card.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    export let imageUrl: string = '';
+</script>
+
+<div class="card">
+    <slot name="card-head" title="show more...">
+        <h2>card head</h2>
+    </slot>
+
+    <slot name="card-image" {imageUrl}>
+        <!-- <img src={imageUrl} alt="" class="image" /> -->
+        <!-- <img src={imageUrl} alt="" class="image" /> -->
+        <div class="image" style="background-image: url({imageUrl});" />
+    </slot>
+
+    <slot name="card-body">
+        <h2>card body</h2>
+    </slot>
+
+    <slot name="card-footer">
+        <h2>card footer</h2>
+    </slot>
+</div>
+
+<style>
+    .card {
+        width: 20rem;
+        background-color: lightgray;
+        padding: 1rem;
+        border-radius: 8px;
+    }
+
+    .image {
+        height: 10rem;
+        background-position: center;
+        background-size: cover;
+        background-repeat: no;
+        margin: 1rem;
+    }
+</style>
