50d9d12229ffed8227cf6e37c3ed6586ddaaafc4
Tina.Lin <Tinalin.Lin@advantech.com.tw>
2021-02-18 15:27:25 +0800

    Add | modals meetup interface

---
+++ src/modals/meetup/meetup.ts
@@ -0,0 +1,9 @@
+export interface IMeetupItem {
+    id: string;
+    title: string;
+    subTitle: string;
+    description: string;
+    imageUrl: string;
+    address: string;
+    contactEmail: string;
+}
