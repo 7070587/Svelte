6fa090ab6e675e6dddf5dede15d5186e916daa22
Tina.Lin <Tinalin.Lin@advantech.com.tw>
2021-02-19 10:34:50 +0800

    Update | enums meetup.ts | add enum

--- src/enums/meetup.ts
+++ src/enums/meetup.ts
@@ -1,8 +1,13 @@
-export enum EFormType {
-    // base type
+export enum EInputType {
     number = 'number',
     text = 'text',
     email = 'email',
     password = 'password',
     textarea = 'textarea',
+}
+
+export enum EButtonType {
+    button = 'button',
+    submit = 'submit',
+    reset = 'reset',
 }
