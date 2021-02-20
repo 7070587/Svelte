df829850ddb3c0647af6028275198b21c8bd817d
Tina.Lin <Tinalin.Lin@advantech.com.tw>
2021-02-20 14:45:52 +0800

    Add | Badge.svelte

---
+++ src/UI/Badge.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<span>
+    <slot />
+</span>
+
+<style>
+    span {
+        display: inline-block;
+        margin: 0.25rem;
+        border-radius: 4px;
+        border: 1px solid $colorMain;
+        background: $colorMain;
+        color: #fff;
+        padding: 0 0.5rem 0.15rem;
+        font-family: 'Lato', sans-serif;
+        font-size: 0.8rem;
+    }
+</style>
