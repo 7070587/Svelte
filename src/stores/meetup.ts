4833e134245ff4ac1da1a54de554dc158b7d8bfb
Tina.Lin <Tinalin.Lin@advantech.com.tw>
2021-02-25 15:19:30 +0800

    Update | store/meetup | 1. remove original fake data     2. add  setMeetup function

--- src/stores/meetup.ts
+++ src/stores/meetup.ts
@@ -6,32 +6,35 @@ import type { IMeetup } from './../modals';
 import { ServiceGenerate } from './../helper';
 
 const meetups: Writable<IMeetup.IMeetupItem[]> = writable([
-    {
-        id: ServiceGenerate.randomId(),
-        title: 'svelte_title',
-        subTitle: 'svelte_subTitle',
-        description:
-            'svelte_description svelte_description svelte_description svelte_description svelte_description svelte_description svelte_description svelte_description svelte_description svelte_description ',
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/03/26/09/48/chicago-690364_960_720.jpg',
-        address: 'svelte_address',
-        contactEmail: 'svelte@svelte.svelte',
-        isFavorite: true,
-    },
-    {
-        id: ServiceGenerate.randomId(),
-        title: 'svelte_title2',
-        subTitle: 'svelte_subTitle2',
-        description: 'svelte_description2',
-        imageUrl: 'https://cdn.pixabay.com/photo/2016/01/19/17/29/earth-1149733_960_720.jpg',
-        address: 'svelte_address2',
-        contactEmail: 'svelte2@svelte.svelte',
-        isFavorite: false,
-    },
+    // {
+    //     id: ServiceGenerate.randomId(),
+    //     title: 'svelte_title',
+    //     subTitle: 'svelte_subTitle',
+    //     description:
+    //         'svelte_description svelte_description svelte_description svelte_description svelte_description svelte_description svelte_description svelte_description svelte_description svelte_description ',
+    //     imageUrl: 'https://cdn.pixabay.com/photo/2015/03/26/09/48/chicago-690364_960_720.jpg',
+    //     address: 'svelte_address',
+    //     contactEmail: 'svelte@svelte.svelte',
+    //     isFavorite: true,
+    // },
+    // {
+    //     id: ServiceGenerate.randomId(),
+    //     title: 'svelte_title2',
+    //     subTitle: 'svelte_subTitle2',
+    //     description: 'svelte_description2',
+    //     imageUrl: 'https://cdn.pixabay.com/photo/2016/01/19/17/29/earth-1149733_960_720.jpg',
+    //     address: 'svelte_address2',
+    //     contactEmail: 'svelte2@svelte.svelte',
+    //     isFavorite: false,
+    // },
 ]);
 
 const meetupsStore = {
     subscribe: meetups.subscribe,
-    saveData: (meetupData: IMeetup.IMeetupItem) => {
+    setMeetup: (meetupsArray: IMeetup.IMeetupItem[]) => {
+        meetups.set(meetupsArray);
+    },
+    saveMeetup: (meetupData: IMeetup.IMeetupItem) => {
         const newMeetup: IMeetup.IMeetupItem = { ...meetupData };
         meetups.update((items: IMeetup.IMeetupItem[]): IMeetup.IMeetupItem[] => {
             return [newMeetup, ...items];
