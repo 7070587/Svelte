eb58e211ad1178cf67517623c9abdbd360d97ab4
7070587 <7070587@gmail.com>
2021-03-01 18:27:21 +0800

    Update | App.svelte | update show loading timing

--- src/App.svelte
+++ src/App.svelte
@@ -31,12 +31,13 @@
     async function getAPIData(): Promise<void> {
         isLoadong = true;
         let res: any = await fetch('https://svelte-meeup-default-rtdb.firebaseio.com/meetups.json').catch((err) => {
-            errorData = err;
+            isLoadong = false;
+            errorMessage = err;
             errorMessage = err ?? errorMessage;
             console.error(err);
         });
 
-        isLoadong = false;
+        if (res) isLoadong = false;
         if (!res.ok) throw new Error('Fetch data error occured, please try again');
         let data: IMeetup.IMeetupItem = await res.json();
         const loadMeetups: IMeetup.IMeetupItem[] = [];
@@ -44,10 +45,7 @@
             loadMeetups.push({ ...data[key], id: key });
         }
 
-        setTimeout(() => {
-            isLoadong = false;
-            meetups.setMeetup(loadMeetups.reverse());
-        }, 1000);
+        meetups.setMeetup(loadMeetups.reverse());
     }
 
     function saveMeetup(): void {
